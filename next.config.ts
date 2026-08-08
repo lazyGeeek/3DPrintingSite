4c63ee6cb9b5d270e96ece3344c90c241b894cb5
lazyGeeek <johndoemailbox2@gmail.com>
2026-08-08 17:16:03 +0300

    Update GitHub Actions

--- next.config.ts
+++ next.config.ts
@@ -1,33 +1,26 @@
 import type { NextConfig } from "next";
 
-const isExport = process.env.NEXT_OUTPUT === "export";
-
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
   output: 'export',        // Enables static export to `out/`
-  distDir: '.next',        // Default; keep unless you changed it
+  // distDir: '.next',        // Default; keep unless you changed it
   images: {
     unoptimized: true,     // Required for GitHub Pages (no Next.js Image optimizer)
   },
-  ...(isExport ? { output: "export" } : {}),
-  ...(isExport
-    ? {}
-    : {
-        async headers() {
-          return [
-            {
-              source: "/:path*",
-              headers: [
-                {
-                  key: "X-Robots-Tag",
-                  value: "noindex, nofollow, noarchive, nosnippet, noimageindex",
-                },
-              ],
-            },
-          ];
-        },
-      }),
+  // async headers() {
+  //   return [
+  //     {
+  //       source: '/:path*',
+  //       headers: [
+  //         {
+  //           key: 'X-Robots-Tag',
+  //           value: 'noindex, nofollow, noarchive, nosnippet, noimageindex',
+  //         },
+  //       ],
+  //     },
+  //   ]
+  // },
 };
 
 export default nextConfig;
